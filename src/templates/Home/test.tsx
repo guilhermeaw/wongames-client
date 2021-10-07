8b3ea1db40f9002084971bb26e277062dfe13a10
guilhermeaw <guilherme.w@universo.univates.br>
2021-10-06 23:34:57 -0300

    feat: Add home page component

---
+++ src/templates/Home/test.tsx
@@ -0,0 +1,47 @@
+import 'match-media-mock'
+import { screen } from '@testing-library/react'
+
+import { renderWithTheme } from 'utils/tests/helpers'
+import bannerMock from 'components/BannerSlider/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+
+const props = {
+  banners: bannerMock,
+  newGames: gamesMock,
+  mostPopularHighlight: highlightMock,
+  mostPopularGames: gamesMock,
+  upcommingGames: gamesMock,
+  upcommingHighligth: highlightMock,
+  upcommingMoreGames: gamesMock,
+  freeGames: gamesMock,
+  freeHighligth: highlightMock
+}
+
+import Home from '.'
+
+describe('<Home />', () => {
+  it('should render menu and footer', () => {
+    renderWithTheme(<Home {...props} />)
+
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /contact us/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render the sections', () => {
+    renderWithTheme(<Home {...props} />)
+
+    expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /most popular/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /upcoming/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /free games/i })
+    ).toBeInTheDocument()
+  })
+})
