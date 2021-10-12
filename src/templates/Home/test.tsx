c978202e687456f58e639d462436b03bc570af3b
guilhermeaw <guilherme.w@universo.univates.br>
2021-10-12 19:56:57 -0300

    test: Refactor home page tests

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -5,33 +5,34 @@ import { renderWithTheme } from 'utils/tests/helpers'
 import bannerMock from 'components/BannerSlider/mock'
 import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
+import Home from '.'
 
 const props = {
   banners: bannerMock,
-  newGames: gamesMock,
+  newGames: [gamesMock[0]],
   mostPopularHighlight: highlightMock,
-  mostPopularGames: gamesMock,
-  upcommingGames: gamesMock,
+  mostPopularGames: [gamesMock[0]],
+  upcommingGames: [gamesMock[0]],
   upcommingHighligth: highlightMock,
-  upcommingMoreGames: gamesMock,
-  freeGames: gamesMock,
+  upcommingMoreGames: [gamesMock[0]],
+  freeGames: [gamesMock[0]],
   freeHighligth: highlightMock
 }
-
-import Home from '.'
 
 describe('<Home />', () => {
   it('should render menu and footer', () => {
     renderWithTheme(<Home {...props} />)
 
+    //menu
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
-    expect(
-      screen.getByRole('heading', { name: /contact us/i })
-    ).toBeInTheDocument()
-  })
 
-  it('should render the sections', () => {
-    renderWithTheme(<Home {...props} />)
+    //footer
+    expect(
+      screen.getByRole('heading', { name: /follow us/i })
+    ).toBeInTheDocument()
+
+    //logos (menu/footer)
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
 
     expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument()
     expect(
@@ -43,5 +44,12 @@ describe('<Home />', () => {
     expect(
       screen.getByRole('heading', { name: /free games/i })
     ).toBeInTheDocument()
+
+    // banner
+    expect(screen.getAllByText(/defy death 1/i)).toHaveLength(1)
+    // card game ( 5 sections com 1 card cada = 5x1 = 5)
+    expect(screen.getAllByText(/population zero/i)).toHaveLength(5)
+    // highlight
+    expect(screen.getAllByText(/read dead is back!/i)).toHaveLength(3)
   })
 })
