53eb75067b9905b820dede904dbeec571db73ab7
guilhermeaw <guilherme.w@universo.univates.br>
2021-10-22 22:07:26 -0300

    feat: Add onCheck prop to Checkbox

--- src/components/Checkbox/test.tsx
+++ src/components/Checkbox/test.tsx
@@ -1,6 +1,7 @@
-import { screen } from '@testing-library/react'
-import { renderWithTheme } from 'utils/tests/helpers'
+import { screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
+import { renderWithTheme } from 'utils/tests/helpers'
 import Checkbox from '.'
 
 describe('<Checkbox />', () => {
@@ -25,6 +26,18 @@ describe('<Checkbox />', () => {
 
     expect(screen.getByText(/checkbox label/i)).toHaveStyle({
       color: '#030517'
+    })
+  })
+
+  it('should dispatch onCheck when status changes', async () => {
+    const onCheck = jest.fn()
+
+    renderWithTheme(<Checkbox label="checkbox label" onCheck={onCheck} />)
+    expect(onCheck).not.toHaveBeenCalled()
+
+    userEvent.click(screen.getByRole('checkbox'))
+    await waitFor(() => {
+      expect(onCheck).toHaveBeenCalledTimes(1)
     })
   })
 })
