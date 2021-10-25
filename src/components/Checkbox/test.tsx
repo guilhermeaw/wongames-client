bdbf8ceef912247f5d988057455ae83ee28d3a40
guilhermeaw <guilherme.w@universo.univates.br>
2021-10-24 22:57:57 -0300

    feat: Finish checkbox component

--- src/components/Checkbox/test.tsx
+++ src/components/Checkbox/test.tsx
@@ -39,5 +39,29 @@ describe('<Checkbox />', () => {
     await waitFor(() => {
       expect(onCheck).toHaveBeenCalledTimes(1)
     })
+    expect(onCheck).toHaveBeenCalledWith(true)
+  })
+
+  it('should dispatch onCheck when status changes', async () => {
+    const onCheck = jest.fn()
+
+    renderWithTheme(
+      <Checkbox label="checkbox label" onCheck={onCheck} isChecked />
+    )
+    expect(onCheck).not.toHaveBeenCalled()
+
+    userEvent.click(screen.getByRole('checkbox'))
+    await waitFor(() => {
+      expect(onCheck).toHaveBeenCalledTimes(1)
+    })
+    expect(onCheck).toHaveBeenCalledWith(false)
+  })
+
+  it('should be accessible with tab', () => {
+    renderWithTheme(<Checkbox label="checkbox" labelFor="checkbox" />)
+    expect(document.body).toHaveFocus()
+
+    userEvent.tab()
+    expect(screen.getByLabelText(/checkbox/i))
   })
 })
