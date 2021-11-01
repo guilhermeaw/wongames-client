5a0833f4ee36a9b0c2d272b05a888e4f4244911f
guilhermeaw <guilherme.w@universo.univates.br>
2021-10-31 22:57:19 -0300

    feat: Add auth template

---
+++ src/templates/Auth/test.tsx
@@ -0,0 +1,46 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Auth from '.'
+
+describe('<Auth />', () => {
+  it('should render all components and children', () => {
+    renderWithTheme(
+      <Auth title="Auth title">
+        <input type="text" />
+      </Auth>
+    )
+
+    // verifica se tem 2 logos
+    expect(screen.getAllByRole('img', { name: /Won Games/i })).toHaveLength(2)
+
+    // verifica se tem o heading principal do banner
+    expect(
+      screen.getByRole('heading', {
+        name: /All your favorite games in one place/i
+      })
+    ).toBeInTheDocument()
+
+    // verifica se tem o subtitle
+    expect(
+      screen.getByRole('heading', {
+        name: /WON is the best and most complete gaming platform./i
+      })
+    ).toBeInTheDocument()
+
+    // verifica se tem o footer
+    expect(
+      screen.getByText(/Won Games 2021 © Todos os Direitos Reservados/i)
+    ).toBeInTheDocument()
+
+    // verifica se tem o title do content
+    expect(
+      screen.getByRole('heading', {
+        name: /Auth title/i
+      })
+    ).toBeInTheDocument()
+
+    // verifica se o children está sendo renderizado
+    expect(screen.getByRole('textbox')).toBeInTheDocument()
+  })
+})
