008b69fd890f8ed0f66bbe0a61fa8b3c5c4caad2
guilhermeaw <guilherme.w@universo.univates.br>
2021-11-09 22:09:18 -0300

    feat: Add huge size to heading component

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -38,10 +38,9 @@ describe('<Heading />', () => {
 
   it('should render a heading with a small size', () => {
     renderWithTheme(<Heading size="small">Won Games</Heading>)
-    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
-      'font-size',
-      '1.6rem'
-    )
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      'font-size': '1.6rem'
+    })
 
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
       'width',
@@ -50,6 +49,13 @@ describe('<Heading />', () => {
         modifier: '::after'
       }
     )
+  })
+
+  it('should render a heading with a huge size', () => {
+    renderWithTheme(<Heading size="huge">Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      'font-size': '5.2rem'
+    })
   })
 
   it('should render a heading with a primary line color', () => {
