84328cf498bab5834717c4e16e926a052dd39005
guilhermeaw <guilherme.w@universo.univates.br>
2023-01-29 22:10:35 -0300

    Renders empty cartlist if there are no items

--- src/components/CartList/test.tsx
+++ src/components/CartList/test.tsx
@@ -20,4 +20,11 @@ describe('<CartList />', () => {
     renderWithTheme(<CartList items={mockItems} total="R$ 330,00" hasButton />)
     expect(screen.getByText(/buy it now/i)).toBeInTheDocument()
   })
+
+  it('should render empty if there are no games', () => {
+    renderWithTheme(<CartList />)
+
+    expect(screen.getByText(/your cart is empty/i)).toBeInTheDocument()
+    expect(screen.queryByText(/total/i)).not.toBeInTheDocument()
+  })
 })
